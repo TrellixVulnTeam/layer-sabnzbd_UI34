b65c706160cadf8935787da1b1b47054104ab1aa
Chris Sanders <sanders.chris@gmail.com>
2017-04-08 14:45:55 -0500

    Added build scripts and started install

--- reactive/sabnzbd.py
+++ reactive/sabnzbd.py
@@ -1,17 +1,9 @@
 from charms.reactive import when, when_not, set_state
+from charmhelpers.fetch import apt_install add_source
 
-
-@when_not('layer-sabnzbd.installed')
+@when_not('sabnzbd.installed')
 def install_layer_sabnzbd():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
-    set_state('layer-sabnzbd.installed')
+   status_set('maintenance','installing sabnzbd')
+   add_source('multiverse')
+   apt_install('sabnzbdplus')
+   set_state('sabnzbd.installed')
