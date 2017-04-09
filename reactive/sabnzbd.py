4886cf9a16ad1f00d732b1dd81b5ee92dcdbc7f2
Chris Sanders <sanders.chris@gmail.com>
2017-04-09 17:56:54 -0500

    Restore from backup configuration

--- reactive/sabnzbd.py
+++ reactive/sabnzbd.py
@@ -1,27 +1,56 @@
 from charms.reactive import when, when_all, when_not, set_state
 from charmhelpers.core import hookenv
 from charmhelpers.fetch import apt_install, add_source
-from charmhelpers.core.host import adduser, service_start, service_restart
-from charmhelpers.core.hookenv import status_set, log
+from charmhelpers.core.host import adduser, service_start, service_restart, chownr
+from charmhelpers.core.hookenv import status_set, log, resource_get
+import os
 import random
 import string
 import fileinput
 import socket
+import tarfile
 
 @when_not('sabnzbd.installed')
 def install_sabnzbd():
     config = hookenv.config()
     status_set('maintenance','installing sabnzbd')
     add_source('multiverse')
-    adduser(config['sabuser'],password=''.join([random.choice(string.printable) for _ in range(random.randint(8, 12))]),home_dir='/home/'+config['sabuser'])
+    adduser(config['sabuser'],password=''''''.join([random.choice(string.printable) for _ in range(random.randint(8, 12))]),home_dir='/home/'+config['sabuser'])
     apt_install('python-openssl')
     apt_install('sabnzbdplus')
     status_set('active','')
     set_state('sabnzbd.installed')
 
 @when_all('sabnzbd.installed','layer-hostname.installed')
+@when_not('sabnzbd.restored')
+def restore_user_conf():
+    config = hookenv.config()
+    backups = './backups'
+    if config['restore-config']:
+        try:
+            os.mkdir(backups)
+        except OSError as e:
+            if e.errno is 17:
+              pass
+        backupFile = resource_get('sabconfig')
+        if backupFile:
+            with tarfile.open(backupFile,'r:gz') as inFile:
+                inFile.extractall('/home/{}'.format(config['sabuser']))
+            for line in fileinput.input('/home/{}/.sabnzbd/sabnzbd.ini'.format(config['sabuser']), inplace=True):
+                if line.startswith("host="):
+                    line = "host={}\n".format(socket.gethostname())
+                print(line,end='') # end statement to avoid inserting new lines at the end of the line
+            log("Changing configuration for new host but not ports. The backup configuration will override charm port settings!",'WARNING')
+            chownr('/home/{}/.sabnzbd'.format(config['sabuser']),owner=config['sabuser'],group=config['sabuser'])
+        else:
+            log("Add sabconfig resource, see juju attach or disable restore-config",'ERROR')
+            raise ValueError('Sabconfig resource missing, see juju attach or disable restore-config')
+    set_state('sabnzbd.restored')
+        
+
+@when_all('sabnzbd.restored')
 @when_not('sabnzbd.configured')
-def wrtie_congs():
+def write_configs():
     config = hookenv.config()
     status_set('maintenance','configuring sabnzbd')
     address = socket.gethostname()
@@ -36,7 +65,7 @@ def wrtie_congs():
     hookenv.open_port(config['port'],'TCP')
     service_restart('sabnzbdplus')
     status_set('active','')
+    set_state('sabnzbd.configured')
     set_state('sabnzbd.ready')
 
-# TODO add config restore from backup
 # TODO add relations
