2bdde54ae38c14b77d9e6611c55e0213082aeb55
Chris Sanders <sanders.chris@gmail.com>
2017-08-23 21:20:08 -0500

    Split library, added backup, and reerseproxy interface

---
+++ lib/libsab.py
@@ -0,0 +1,88 @@
+from charmhelpers.core import hookenv, host
+
+import configparser
+import fileinput
+import tarfile
+import socket
+import os
+
+
+class SabInfo:
+    def __init__(self):
+        self.charm_config = hookenv.config()
+        self.user = self.charm_config['sabuser']
+        self.home_dir = '/home/{}'.format(self.user)
+        self.install_dir = self.home_dir + '/.sabnzbd'
+        self.settings_file = self.install_dir + '.sabnzbd.ini'
+        self.default_file = "/etc/default/sabnzbdplus"
+        self.resource_name = 'sabconfig'
+        self.resource_folder = hookenv.charm_dir() + '/resources'
+        self.sab_config = configparser.ConfigParser()
+        self.sab_config.read(self.settings_file)
+
+    def reload_config(self):
+        self.sab_config.read(self.settings_file)
+
+    def save_config(self):
+        with open(self.settings_file, 'w') as openFile:
+            self.sab_config.write(openFile)
+
+    def add_user(self):
+        host.adduser(self.charm_config['sabuser'], password="", shell='/bin/False', home_dir=self.home_dir)
+
+    def set_host(self):
+        self.sab_config['misc']['host'] = self.host
+        hookenv.log("Couchpotato hostname set to {}".format(self.host), "INFO")
+        # for line in fileinput.input(self.settings_file, inplace=True):
+        #     if line.startswith("host="):
+        #         line = "host={}\n".format(self.host)
+        #     print(line, end='')  # end statement to avoid inserting new lines at the end of the line
+
+    def set_port(self):
+        self.sab_config['misc']['port'] = self.charm_config['port']
+
+    def set_defaults(self):
+        for line in fileinput.input(self.default_file, inplace=True):
+            if line.startswith("USER="):
+                line = "USER={}\n".format(self.charm_config['sabuser'])
+            if line.startswith("HOST="):
+                line = "HOST={}\n".format(self.host)
+            if line.startswith("PORT=\n"):
+                line = "PORT={}".format(self.charm_config['port'])
+            print(line, end='')  # end statement to avoid inserting new lines at the end of the line
+
+    def restore_config(self):
+        try:
+            os.mkdir(self.resource_folder)
+        except OSError as e:
+            if e.errno is 17:
+                pass
+
+        backupFile = hookenv.resource_get(self.resource_name)
+        if backupFile:
+            with tarfile.open(backupFile, 'r:gz') as inFile:
+                inFile.extractall(self.home_dir)
+            self.set_host() 
+            self.set_port()
+            self.save_config()
+            host.chownr(self.install_dir, owner=self.charm_config['sabuser'], group=self.charm_config['sabuser'])
+            return True
+        else:
+            hookenv.log("Add sabconfig resource, see juju attach or disable restore-config", 'ERROR')
+            hookenv.status_set('blocked', 'Waiting on sabconfig resource')
+            return False
+
+    @property
+    def api_key(self):
+        return self.sab_config['misc']['api_key']
+        # for line in fileinput.input(self.settings_file):
+        #     if line.startswith("api_key"):
+        #         apikey = line.split("=")[1].strip()
+        # return apikey
+
+    @property
+    @hookenv.cached
+    def host(self):
+        return socket.getfqdn()
+        # return socket.gethostname()
+        # TODO: should this use socket.getfqdn instead of just gethostname?
--- reactive/sabnzbd.py
+++ reactive/sabnzbd.py
@@ -1,87 +1,93 @@
 from charms.reactive import when, when_all, when_not, set_state
 from charmhelpers.core import hookenv
-from charmhelpers.fetch import apt_install, add_source, apt_update
-from charmhelpers.core.host import adduser, service_start, service_restart, chownr
-from charmhelpers.core.hookenv import status_set, log, resource_get
-import os
-import random
-import string
+from charmhelpers import fetch
+from charmhelpers.core import host
 import fileinput
-import socket
-import tarfile
+
+from libsab import SabInfo
+
+sab = SabInfo()
+
 
 @when_not('sabnzbd.installed')
 def install_sabnzbd():
-    config = hookenv.config()
-    status_set('maintenance','installing sabnzbd')
-    add_source('ppa:jcfp/nobetas')
-    add_source('ppa:jcfp/sab-addons')
-    apt_update()
-    adduser(config['sabuser'],password="",shell='/bin/False',home_dir='/home/'+config['sabuser'])
-    apt_install('python-openssl')
-    apt_install('par2-tbb')
-    apt_install('python-sabyenc')
-    apt_install('sabnzbdplus')
-    status_set('active','')
+    hookenv.status_set('maintenance', 'installing sabnzbd')
+    fetch.add_source('ppa:jcfp/nobetas')
+    fetch.add_source('ppa:jcfp/sab-addons')
+    fetch.apt_update()
+    sab.add_user()
+    fetch.apt_install('python-openssl')
+    fetch.apt_install('par2-tbb')
+    fetch.apt_install('python-sabyenc')
+    fetch.apt_install('sabnzbdplus')
+    hookenv.status_set('active', '')
     set_state('sabnzbd.installed')
 
-@when_all('sabnzbd.installed','layer-hostname.installed')
+
+@when_all('sabnzbd.installed', 'layer-hostname.installed')
 @when_not('sabnzbd.restored')
 def restore_user_conf():
-    config = hookenv.config()
-    backups = './backups'
-    if config['restore-config']:
-        try:
-            os.mkdir(backups)
-        except OSError as e:
-            if e.errno is 17:
-              pass
-        backupFile = resource_get('sabconfig')
-        if backupFile:
-            with tarfile.open(backupFile,'r:gz') as inFile:
-                inFile.extractall('/home/{}'.format(config['sabuser']))
-            for line in fileinput.input('/home/{}/.sabnzbd/sabnzbd.ini'.format(config['sabuser']), inplace=True):
-                if line.startswith("host="):
-                    line = "host={}\n".format(socket.gethostname())
-                print(line,end='') # end statement to avoid inserting new lines at the end of the line
-            log("Changing configuration for new host but not ports. The backup configuration will override charm port settings!",'WARNING')
-            chownr('/home/{}/.sabnzbd'.format(config['sabuser']),owner=config['sabuser'],group=config['sabuser'])
-        else:
-            log("Add sabconfig resource, see juju attach or disable restore-config",'ERROR')
-            status_set('blocked','Waiting on sabconfig resource')
-            return
+    if sab.charm_config['restore-config']:
+        if not sab.restore_config():
+            return  # If restore failed exit with out setting the state
     set_state('sabnzbd.restored')
         
 
 @when_all('sabnzbd.restored')
 @when_not('sabnzbd.configured')
 def write_configs():
-    config = hookenv.config()
-    status_set('maintenance','configuring sabnzbd')
-    address = socket.gethostname()
-    for line in fileinput.input('/etc/default/sabnzbdplus', inplace=True):
-        if line.startswith("USER="):
-            line = "USER={}\n".format(config['sabuser'])
-        if line.startswith("HOST="):
-            line = "HOST={}\n".format(address)
-        if line.startswith("PORT=\n"):
-            line = "PORT={}".format(config['port'])
-        print(line,end='') # end statement to avoid inserting new lines at the end of the line
-    hookenv.open_port(config['port'],'TCP')
-    service_restart('sabnzbdplus')
-    status_set('active','')
+    # config = hookenv.config()
+    hookenv.status_set('maintenance', 'configuring sabnzbd')
+    sab.set_defaults()
+    hookenv.open_port(sab.charm_config['port'], 'TCP')
+    host.service_restart('sabnzbdplus')
+    hookenv.status_set('active', '')
     set_state('sabnzbd.configured')
     set_state('sabnzbd.ready')
 
+
+@when('sabnzbd.installed')
+@when_not('avahi.configured')
+def configure_avahi():
+    for line in fileinput.input('/etc/avahi/avahi-daemon.conf', inplace=True):
+        if line.startswith("rlimit-nproc"):
+            line = "#" + line
+        print(line, end='')
+    set_state('avahi.configured')
+
+
 @when_not('usenet-downloader.configured')
-@when_all('usenet-downloader.triggered','sabnzbd.configured')
+@when_all('usenet-downloader.triggered', 'sabnzbd.configured')
 def configure_interface(usenetdownloader):
-    config = hookenv.config()
-    hostname = socket.gethostname()
-    port = config['port']
-    for line in fileinput.input('/home/{}/.sabnzbd/sabnzbd.ini'.format(config['sabuser'])):
-        if line.startswith("api_key"):
-            apikey = line.split("=")[1].strip()
-    usenetdownloader.configure(hostname=hostname,port=port,apikey=apikey)
-    log('usenet download provider configured','INFO')
+    hostname = sab.host
+    port = sab.charm_config['port']
+    apikey = sab.apikey
+    usenetdownloader.configure(hostname=hostname, port=port, apikey=apikey)
+    hookenv.log('usenet download provider configured', 'INFO')
+
+
+@when_all('reverseproxy.triggered', 'reverseproxy.ready')
+@when_not('reverseproxy.configured', 'reverseproxy.departed')
+def configure_reverseproxy(reverseproxy, *args):
+    # TODO: retrigger if config changes?
+    hookenv.log("Setting up reverseproxy", "INFO")
+    proxy_info = {'urlbase': sab.charm_config['proxy-url'],
+                  'subdomain': sab.charm_config['proxy-domain'],
+                  'group_id': 'sabnzbd',
+                  'external_port': sab.charm_config['proxy-port'],
+                  'internal_host': sab.host,
+                  'internal_port': sab.charm_config['port']
+                  } 
+
+    reverseproxy.configure(proxy_info)
+    # cp.set_urlbase(proxy_info['urlbase'])
+    # cp.restart()
+
+
+@when_all('reverseproxy.triggered', 'reverseproxy.departed')
+def remove_urlbase(reverseproxy, *args):
+    hookenv.log("Removing reverseproxy configuration", "INFO")
+    # cp.set_urlbase('')
+    # cp.restart()
+
 
